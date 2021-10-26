2d8e3c339d628821bdd7ac4dcabdfa466f68a029
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2021-10-27 07:28:34 +0900

    railsの本番環境用Dockerfileを作成 #4

---
+++ api/entrypoint_prod.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /api/tmp/pids/server.pid
+
+rails db:create RAILS_ENV=production
+rails db:migrate RAILS_ENV=production
+
+
+# Then exec the container's main process (what's set as CMD in the Dockerfile).
+exec "$@"

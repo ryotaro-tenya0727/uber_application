252a196a1e78f0b986bfba253909ff4aecb7e4c8
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2021-12-03 07:54:29 +0900

    test

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_02_223310) do
+ActiveRecord::Schema.define(version: 2021_10_26_085307) do
 
   create_table "issues", charset: "utf8mb4", force: :cascade do |t|
     t.text "name"

778e000c151eb38507336657c97dbaa77b50aa4b
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2021-12-03 07:20:01 +0900

    first

--- front/reactapp/src/App.js
+++ front/reactapp/src/App.js
@@ -1,11 +1,6 @@
-import React, { useEffect } from 'react';
-import { fetch } from './components/Todo';
 import './App.css';
 
 function App() {
-  useEffect(() => {
-    fetch().then((data) => console.log(data));
-  }, []);
   return <div className='App'>test</div>;
 }
 

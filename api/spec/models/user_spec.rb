f9664baa63a0416de2f349669f7f7f4ce192b8ac
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2021-12-08 11:41:31 +0900

    【追加】 Modelファイルに関連づけを追加する
    中間テーブルを用いた関連づけをする必要があった。
    
    Issue #4

--- api/spec/models/user_spec.rb
+++ api/spec/models/user_spec.rb
@@ -1,16 +1,16 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe User, type: :model do
   # 姓、名、メール、パスワードがあれば有効な状態であること
-  it 'is valid with a first name, last name, email, and password'
+  it "is valid with a first name, last name, email, and password"
   # 名がなければ無効な状態であること
-  it 'is invalid without a first name'
+  it "is invalid without a first name"
   # 姓がなければ無効な状態であること
-  it 'is invalid without a last name'
+  it "is invalid without a last name"
   # メールアドレスがなければ無効な状態であること
-  it 'is invalid without an email address'
+  it "is invalid without an email address"
   # 重複したメールアドレスなら無効な状態であること
-  it 'is invalid with a duplicate email address'
+  it "is invalid with a duplicate email address"
   # ユーザーのフルネームを文字列として返すこと
   it "returns a user's full name as a string"
 end

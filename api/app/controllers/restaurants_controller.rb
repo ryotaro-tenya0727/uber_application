43e53d3c3475e96ee0f32b62beef7d52af5656ba
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2021-12-11 18:17:10 +0900

    【修正】ルーティング少しだけ変更

--- api/app/controllers/restaurants_controller.rb
+++
@@ -1,9 +0,0 @@
-class RestaurantsController < ApplicationController
-  def index
-    restaurants = Restaurant.all
-
-    render json: {
-      restaurants: restaurants
-    }, status: :ok
-  end
-end

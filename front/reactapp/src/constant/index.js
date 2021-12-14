9fc772747075bc03ae8871ef8612875fb3d5aa08
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2021-12-14 20:56:21 +0900

    【追加】定数と画像を配置#26

---
+++ front/reactapp/src/constant/index.js
@@ -0,0 +1,4 @@
+export { default as REQUEST_STATE } from './constants';
+export { default as HTTP_STATUS_CODE } from './constants';
+export { default as COLORS } from './style_constants';
+export { default as FONT_SIZE } from './style_constants';
